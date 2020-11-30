c6527667f5d9d50c92da26c94f222f9f2a4377ed
Ronnie Feng <ronnie@ronniefeng.com>
2020-11-29 19:35:34 -0500

    setting up folders and files, adding packages

---
+++ src/Views/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <div>
+            about
+        </div>
+    )
+}
+
+export {About}
---
+++ src/Views/Works.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Works = () =>{
+    return(
+        <div>
+            Works
+        </div>
+    )
+}
+
+export {Works}
