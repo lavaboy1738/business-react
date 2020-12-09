f11b3a51a894aa7c7d60fdb615db3aac52b7790c
Ronnie Feng <ronnie@ronniefeng.com>
2020-12-09 10:54:38 -0500

    minor adjustments

--- src/Components/LeadershipCard.tsx
+++ src/Components/LeadershipCard.tsx
@@ -33,7 +33,7 @@ const LeadershipCardStyles = styled(motion.div)`
 `
 
 const LeadershipCard = (props: Leader) => {
-    const {element, controls} = useScroll(0);
+    const {element, controls} = useScroll(0.2);
     const {revealAnimation} = useAnimations();
     return (
         <LeadershipCardStyles
