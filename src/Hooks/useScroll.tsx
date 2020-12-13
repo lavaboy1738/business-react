0d4901e91e9ff29bce04134cb0342bb95055b990
Ronnie Feng <ronnie@ronniefeng.com>
2020-12-13 18:03:14 -0500

    optimizing scroll, trigger once instead of count

--- src/Hooks/useScroll.tsx
+++ src/Hooks/useScroll.tsx
@@ -4,17 +4,13 @@ import {useAnimation} from "framer-motion";
 
 export const useScroll = (threshold: number) => {
     const controls = useAnimation();
-    const [element, inView] = useInView({threshold})
-    const count = useRef(false);
+    const [element, inView] = useInView({threshold: threshold, triggerOnce: true})
     useEffect(()=>{
-        if(!count.current){
             if(inView){
                 controls.start("show")
-                count.current = true;
             }else{
                 controls.start("hidden")
             }
-        }
     })
     return {element, controls}
 }
